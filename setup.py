0dfeda098e9502bdaaa1e821faff61a93a831c45
Shadow .. <23f3001987@ds.study.iitm.ac.in>
2025-08-25 13:24:44 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Aryan Thakur",
+    author_email="aryan030803@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
